94d70da72d3c571c6c94a1f86d894bbd1571093f
JinYeob Kim <37360089+Jinyeob@users.noreply.github.com>
2019-12-23 01:01:49 +0900

    first init - basic parsing function, login

---
+++ app/src/main/java/com/example/parsing2/LoginActivity.java
@@ -0,0 +1,61 @@
+package com.example.parsing2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText idView;
+    private EditText passwdView;
+    private Button loginButton;
+    private CheckBox autoLogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        idView = (EditText) findViewById(R.id.editText_id);
+        passwdView = (EditText) findViewById(R.id.editText_passwd);
+        loginButton = (Button) findViewById(R.id.button_login);
+        autoLogin=(CheckBox)findViewById(R.id.checkBox);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id=idView.getText().toString();
+                String passwd=passwdView.getText().toString();
+
+                if(id.length()>0&&passwd.length()>0) {
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.putExtra("id", id);
+                    intent.putExtra("passwd", passwd);
+
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"ID와 PW를 입력해주세요", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        autoLogin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    SaveSharedPreference.setUser(LoginActivity.this, idView.getText().toString(),passwdView.getText().toString());
+                }
+                else{
+                    SaveSharedPreference.clearUser(LoginActivity.this);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/parsing2/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.parsing2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.util.HashMap;
+import java.util.Map;
+import java.net.URL;
+import java.net.URLConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import java.io.BufferedInputStream;
+
+public class MainActivity extends AppCompatActivity {
+    private String htmlPageUrl = "https://community.jbch.org/confirm.php";
+    private TextView textviewHtmlDocument;
+    private String viewPageUrl = "";
+    Bitmap bm;
+    private WebView webView;
+    private String htmlContentInStringFormat = "";
+String ID="";
+String PASSWD="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+
+        textviewHtmlDocument = (TextView) findViewById(R.id.textView);
+        textviewHtmlDocument.setMovementMethod(new ScrollingMovementMethod()); //스크롤 가능한 텍스트뷰로 만들기
+
+        final Button htmlTitleButton = (Button) findViewById(R.id.button);
+        final Button reLogin = (Button) findViewById(R.id.button2);
+
+        Intent loginIntent=getIntent();
+        ID=loginIntent.getStringExtra("id");
+        PASSWD=loginIntent.getStringExtra("passwd");
+        System.out.println("@@@@ ID = "+ID);
+        System.out.println("@@@@ PASSWD = "+PASSWD);
+
+
+        htmlTitleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                jsoupAsyncTask.execute();
+                htmlTitleButton.setEnabled(false);
+                //htmlTitleButton.setVisibility(View.GONE);
+            }
+        });
+
+        reLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Connection.Response res = Jsoup.connect(htmlPageUrl)
+                        .data("user_id", ID)
+                        .data("saveid", "1")
+                        .data("passwd", PASSWD)
+                        .data("mode", "")
+                        .data("go", "yes")
+                        .data("url", "http://community.jbch.org/")
+                        .data("LoginButton", "LoginButton")
+                        .method(Connection.Method.POST)
+                        .execute();
+
+                Map<String, String> loginCookie = res.cookies();
+                Document doc = Jsoup.connect("https://community.jbch.org/")
+                        .cookies(loginCookie)
+                        .get();
+
+                Elements titles = doc.select("div.conbox.active div.condate.font-daum");
+                Elements contentATags = doc.select("div.conbox.active img");
+
+                System.out.println("-------------------------------------------------------------");
+                for (Element e : titles) {
+                    System.out.println("title: " + e.text());
+                    htmlContentInStringFormat += e.text().trim() + "\n";
+                }
+
+                viewPageUrl = contentATags.attr("abs:src");
+                System.out.println(viewPageUrl);
+                System.out.println("-------------------------------------------------------------");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+           textviewHtmlDocument.setText(htmlContentInStringFormat);
+           webView.loadUrl(viewPageUrl);
+        }
+    }
+}
