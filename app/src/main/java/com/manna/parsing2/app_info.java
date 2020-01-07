45064aff78fff99f664f66da0d8a2c21d7d59274
JinYeob Kim <37360089+Jinyeob@users.noreply.github.com>
2020-01-08 00:39:19 +0900

    디자인 개선2

--- app/src/main/java/com/manna/parsing2/app_info.java
+++ app/src/main/java/com/manna/parsing2/app_info.java
@@ -1,8 +1,10 @@
 package com.manna.parsing2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class app_info extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class app_info extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_info);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.myAppName);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{ //toolbar의 back키 눌렀을 때 동작
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
